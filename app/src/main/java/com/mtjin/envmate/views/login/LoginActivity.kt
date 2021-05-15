bcdda610d3c0e801badddfe30552949519fc98d2
jackjackE <tmddjs210@naver.com>
2021-05-15 20:36:57 +0900

    [Feat] SignInActivity.kt, LoginActivity.kt 추가

---
+++ app/src/main/java/com/mtjin/envmate/views/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.mtjin.envmate.views.login
+
+import android.os.Bundle
+import com.mtjin.envmate.R
+import com.mtjin.envmate.base.BaseActivity
+import com.mtjin.envmate.databinding.ActivityLoginBinding
+
+class LoginActivity : BaseActivity<ActivityLoginBinding>(R.layout.activity_login) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
---
+++ app/src/main/java/com/mtjin/envmate/views/sign_up/SignUpActivity.kt
@@ -0,0 +1,13 @@
+package com.mtjin.envmate.views.sign_up
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mtjin.envmate.R
+import com.mtjin.envmate.base.BaseActivity
+import com.mtjin.envmate.databinding.ActivitySignInBinding
+
+class SignUpActivity : BaseActivity<ActivitySignInBinding>(R.layout.activity_sign_up) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
