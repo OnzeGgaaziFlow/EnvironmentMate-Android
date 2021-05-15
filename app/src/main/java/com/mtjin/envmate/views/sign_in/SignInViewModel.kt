4f8b386e2bbe8da3eb6aebf061ae934fbe3af21c
jackjackE <tmddjs210@naver.com>
2021-05-15 20:28:26 +0900

    [Feat] SignInViewModel.kt 회원가입 관련 로직 추가

--- app/src/main/java/com/mtjin/envmate/views/sign_in/SignInViewModel.kt
+++ app/src/main/java/com/mtjin/envmate/views/sign_in/SignInViewModel.kt
@@ -6,10 +6,16 @@ import com.mtjin.envmate.utils.SingleLiveEvent
 
 class SignInViewModel : BaseViewModel() {
     private val _goLogin = SingleLiveEvent<Unit>()
+    private val _goSignUp = SingleLiveEvent<Unit>()
 
     val goLogin: LiveData<Unit> = _goLogin
+    val goSignUp: LiveData<Unit> = _goSignUp
 
     fun goLogin() {
         _goLogin.call()
+    }
+
+    fun goSignUp() {
+        _goSignUp.call()
     }
 }
