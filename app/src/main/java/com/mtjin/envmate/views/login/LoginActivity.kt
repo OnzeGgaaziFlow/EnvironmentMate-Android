e079c0bb83da2a0d16247e33a236651281e1669a
jackjackE <tmddjs210@naver.com>
2021-05-15 20:38:46 +0900

    [Feat] SignInActivity.kt, LoginActivity.kt Hilt 적용

--- app/src/main/java/com/mtjin/envmate/views/login/LoginActivity.kt
+++ app/src/main/java/com/mtjin/envmate/views/login/LoginActivity.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import com.mtjin.envmate.R
 import com.mtjin.envmate.base.BaseActivity
 import com.mtjin.envmate.databinding.ActivityLoginBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class LoginActivity : BaseActivity<ActivityLoginBinding>(R.layout.activity_login) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/com/mtjin/envmate/views/sign_up/SignUpActivity.kt
+++ app/src/main/java/com/mtjin/envmate/views/sign_up/SignUpActivity.kt
@@ -5,7 +5,9 @@ import android.os.Bundle
 import com.mtjin.envmate.R
 import com.mtjin.envmate.base.BaseActivity
 import com.mtjin.envmate.databinding.ActivitySignInBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class SignUpActivity : BaseActivity<ActivitySignInBinding>(R.layout.activity_sign_up) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
