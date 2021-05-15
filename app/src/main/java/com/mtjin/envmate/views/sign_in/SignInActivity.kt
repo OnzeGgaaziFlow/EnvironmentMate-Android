1ea838f0ef318e6a66c58c065ff61e3c134b48e6
jackjackE <tmddjs210@naver.com>
2021-05-15 19:23:23 +0900

    [Feat] SignInActivity.kt, SplashActivity.kt 추가

---
+++ app/src/main/java/com/mtjin/envmate/views/sign_in/SignInActivity.kt
@@ -0,0 +1,12 @@
+package com.mtjin.envmate.views.sign_in
+
+import android.os.Bundle
+import com.mtjin.envmate.R
+import com.mtjin.envmate.base.BaseActivity
+import com.mtjin.envmate.databinding.ActivitySignInBinding
+
+class SignInActivity : BaseActivity<ActivitySignInBinding>(R.layout.activity_sign_in) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
---
+++ app/src/main/java/com/mtjin/envmate/views/splash/SplashActivity.kt
@@ -0,0 +1,24 @@
+package com.mtjin.envmate.views.splash
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import com.mtjin.envmate.R
+import com.mtjin.envmate.views.sign_in.SignInActivity
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        goSignIn()
+    }
+
+    private fun goSignIn() {
+        Handler(Looper.getMainLooper()).postDelayed(Runnable {
+            startActivity(Intent(this, SignInActivity::class.java))
+            finish()
+        }, 1000)
+    }
+}
