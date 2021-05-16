3c9b50abc57ac94fbcdca3e2c230403f0cc8c963
jackjackE <tmddjs210@naver.com>
2021-05-16 18:58:59 +0900

    [Feat] activity_user_info.xml 회원가입-기업및유저 정보 디자인 구현

--- app/src/main/java/com/mtjin/envmate/views/user_info/UserInfoActivity.kt
+++ app/src/main/java/com/mtjin/envmate/views/user_info/UserInfoActivity.kt
@@ -3,10 +3,12 @@ package com.mtjin.envmate.views.user_info
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.mtjin.envmate.R
+import com.mtjin.envmate.base.BaseActivity
+import com.mtjin.envmate.databinding.ActivityUserInfoBinding
 
-class UserInfoActivity : AppCompatActivity() {
+class UserInfoActivity : BaseActivity<ActivityUserInfoBinding>(R.layout.activity_user_info) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_user_info)
+
     }
 }
