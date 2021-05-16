ef10db03c1676982ded40f36addf0704e9be19c9
jackjackE <tmddjs210@naver.com>
2021-05-16 19:59:06 +0900

    [Feat] SignUpCompleteActivity.kt, SignUpCompleteViewModel.kt 회원가입-완료 로직 구현

--- app/src/main/java/com/mtjin/envmate/views/sign_up/sign_up_complete/SignUpCompleteActivity.kt
+++ app/src/main/java/com/mtjin/envmate/views/sign_up/sign_up_complete/SignUpCompleteActivity.kt
@@ -1,13 +1,34 @@
 package com.mtjin.envmate.views.sign_up.sign_up_complete
 
+import android.content.Intent
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.lifecycle.Observer
 import com.mtjin.envmate.R
 import com.mtjin.envmate.base.BaseActivity
-import com.mtjin.envmate.databinding.ActivitySignUpBinding
+import com.mtjin.envmate.databinding.ActivitySignUpCompleteBinding
+import com.mtjin.envmate.views.login.LoginActivity
+import dagger.hilt.android.AndroidEntryPoint
 
+
+@AndroidEntryPoint
 class SignUpCompleteActivity :
-    BaseActivity<ActivitySignUpBinding>(R.layout.activity_sign_up_complete) {
+    BaseActivity<ActivitySignUpCompleteBinding>(R.layout.activity_sign_up_complete) {
+    private val viewModel: SignUpCompleteViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding.vm = viewModel
+        initViewModelCallback()
+    }
+
+    private fun initViewModelCallback() {
+        with(viewModel) {
+            goLogin.observe(this@SignUpCompleteActivity, Observer {
+                val intent = Intent(this@SignUpCompleteActivity, LoginActivity::class.java)
+                intent.flags =
+                    Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP
+                startActivity(intent)
+            })
+        }
     }
 }
