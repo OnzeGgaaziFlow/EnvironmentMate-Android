32853732b2aa26f35da3e0d5ab526859b6632148
jackjackE <tmddjs210@naver.com>
2021-05-16 19:52:20 +0900

    [Feat] activity_sign_up_complete.xml 회원가입-완료 디자인 구현

---
+++ app/src/main/java/com/mtjin/envmate/views/sign_up/sign_up_complete/SignUpCompleteActivity.kt
@@ -0,0 +1,13 @@
+package com.mtjin.envmate.views.sign_up.sign_up_complete
+
+import android.os.Bundle
+import com.mtjin.envmate.R
+import com.mtjin.envmate.base.BaseActivity
+import com.mtjin.envmate.databinding.ActivitySignUpBinding
+
+class SignUpCompleteActivity :
+    BaseActivity<ActivitySignUpBinding>(R.layout.activity_sign_up_complete) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
