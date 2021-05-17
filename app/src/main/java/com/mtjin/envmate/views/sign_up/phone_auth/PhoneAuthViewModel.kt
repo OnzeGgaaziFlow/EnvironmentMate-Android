998320fb98955455dd0be767b69bdcb1f7f176ea
jackjackE <tmddjs210@naver.com>
2021-05-17 18:23:03 +0900

    [Refactor] PhoneAuthViewModel.kt, PhoneAuthActivity.kt 회원가입-폰인증 로직 수정

--- app/src/main/java/com/mtjin/envmate/views/sign_up/phone_auth/PhoneAuthViewModel.kt
+++ app/src/main/java/com/mtjin/envmate/views/sign_up/phone_auth/PhoneAuthViewModel.kt
@@ -9,21 +9,30 @@ import javax.inject.Inject
 
 @HiltViewModel
 class PhoneAuthViewModel @Inject constructor() : BaseViewModel() {
-    var phoneAuthNum: String = ""
+    var isResendPhoneAuth: Boolean = false
+
     val etPhoneNum = MutableLiveData<String>("")
     val etAuthNum = MutableLiveData<String>("")
 
     private val _requestPhoneAuth = MutableLiveData<Boolean>()
+    private val _requestResendPhoneAuth = MutableLiveData<Boolean>()
     private val _authComplete = SingleLiveEvent<Unit>()
 
     val requestPhoneAuth: LiveData<Boolean> get() = _requestPhoneAuth
+    val requestResendPhoneAuth: LiveData<Boolean> get() = _requestResendPhoneAuth
     val authComplete: LiveData<Unit> get() = _authComplete
 
     fun requestPhoneAuth() {
-        _requestPhoneAuth.value = !etPhoneNum.value.isNullOrBlank()
+        if (!isResendPhoneAuth) { //첫 폰인증
+            _requestPhoneAuth.value = !etPhoneNum.value.isNullOrBlank()
+            //isResendPhoneAuth = true
+        } else { //재시도
+            _requestResendPhoneAuth.value = !etPhoneNum.value.isNullOrBlank()
+        }
     }
 
     fun authComplete() {
         _authComplete.call()
     }
+
 }
