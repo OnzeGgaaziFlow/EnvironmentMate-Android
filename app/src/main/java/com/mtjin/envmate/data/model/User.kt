fdc821735cf2180aeac34460e3442b621daa7152
jackjackE <tmddjs210@naver.com>
2021-05-17 18:33:40 +0900

    [Feat] User.kt data class 추가

---
+++ app/src/main/java/com/mtjin/envmate/data/model/User.kt
@@ -0,0 +1,21 @@
+package com.mtjin.envmate.data.model
+
+
+import com.google.gson.annotations.SerializedName
+
+data class User(
+    @SerializedName("business_name")
+    val businessName: String,
+    @SerializedName("business_number")
+    val businessNumber: String,
+    @SerializedName("officer_email")
+    val officerEmail: String,
+    @SerializedName("officer_name")
+    val officerName: String,
+    @SerializedName("officer_phone")
+    val officerPhone: String,
+    @SerializedName("officer_position")
+    val officerPosition: String,
+    @SerializedName("password")
+    val password: String
+)
